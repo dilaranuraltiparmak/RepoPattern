4a629110a8d668b35117ad345c4616ef34e803fa
Dilara Nur Altıparmak <78371701+dilaranuraltiparmak@users.noreply.github.com>
2023-03-21 23:28:43 +0300

    Add project files.

---
+++ LayerDAL/Repository/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayerDAL.Entities;
+using LayerDAL.Setting;
+using Microsoft.Extensions.Options;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LayerDAL.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ConnectionSetting _connection;
+        public EmployeeRepository(IOptions<ConnectionSetting>connection)
+        {
+            _connection = connection.Value;
+        }
+        public async Task<List<Employee>> GetEmployees()
+        {
+            List<Employee> ListEmployees = new List<Employee>();
+            using (var connect = new SqlConnection(_connection.SQLString))
+            {
+                connect.Open();
+                SqlCommand cmd = new SqlCommand("SP_GetEmployees",connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+                using(var reader=await cmd.ExecuteReaderAsync())
+                {
+                    while(await reader.ReadAsync())
+                    {
+                        ListEmployees.Add(new Employee()
+                        {
+                            Id = Convert.ToInt32(reader["EmployeeID"]),
+                            LastName = reader["LastName"].ToString(),
+                            FirstName = reader["FirstName"].ToString(),
+                            Address = reader["Address"].ToString(),
+                            City = reader["City"].ToString()
+
+                        });
+                            
+
+                    }
+                }
+            }
+            return ListEmployees;
+                }
+        }
+    }
+
---
+++ RepositoryPattern/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.Models;
+using System.Diagnostics;
+using LayerDAL.Repository;
+using LayerDAL.Entities;
+
+
+namespace RepositoryPattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _repository;
+
+        public HomeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+     
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ShowEmployees()
+        {
+            List<Employee> ListEmployees = await _repository.GetEmployees();
+            return View(ListEmployees);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
